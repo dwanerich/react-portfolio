63e0639e1e5e99bdd93c9d0cb18f53430e772320
Dwane Richards <drichards@pagerduty.com>
2023-01-04 03:02:07 -0500

    updated bio

--- src/About.js
+++ src/About.js
@@ -27,12 +27,16 @@ const About = () => {
       <main class="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
         <div class="sm:text-center lg:text-left">
           <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-            <span class="block xl:inline">about</span>
+            <span class="block xl:inline">Hello World 👋</span>
             {/* <span class="block text-indigo-600 xl:inline">online business</span> */}
           </h1>
-          <p class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">Dwane is a Software Engineer, 2021 Flatiron School Alum and St John's University Communications graduate.
-          He's a dedicated lifelong learner with experience in Healthcare, Entertainment and SAAS industries. As a Software Engineer, Dwane leverages his creativity and curiosity with web app technologies (JavaScript • React/Redux • Ruby on Rails)
-          to architect solutions for users and businesses. As in life, as a designer, developer and cross-functional collaborator, he’s eager to learn, grow and share what he’s learned with others..</p>
+          <p class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">Software Engineer experienced in Healthcare Information Technology, Entertainment and B2B SaaS.
+
+A relentlessly curious and critical thinker continuously exploring the different ways to leverage technology to architect solutions for users and businesses.
+
+As a designer, developer and cross-functional collaborator, I've gained tremendous value through various experiences by adopting a growth mindset and sharing knowledge with others. <br /><br />
+
+Interests: VR, ML, AI, Web3 & Blockchain Technology </p>
           <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
             <div class="rounded-md shadow">
             <a href="https://drive.google.com/file/d/1qawunH63iFM9ScON1a_Kb8VgwGNiiS9C/view?usp=share_link" target="_blank" class="flex w-full items-center justify-center rounded-md border border-transparent  text-black border-black px-8 py-3 text-base font-medium text-white hover:border-black hover:bg-transparent hover:text-black md:py-4 md:px-10 md:text-lg">resume</a>
